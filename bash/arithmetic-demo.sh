1da513baebe010b465d7db199f44b0a1d67acbfb
jyothish012 <55294428+jyothish012@users.noreply.github.com>
2020-07-08 04:39:35 -0400

    arithmetic-demo.sh

--- bash/arithmetic-demo.sh
+++ bash/arithmetic-demo.sh
@@ -8,6 +8,7 @@
 #   - the output should look something like:
 #   - first divided by second gives X with a remainder of Y
 # Improve this script by calculating and displaying the first number raised to the power of the second number
+#
 
 echo " enter a number : "
 read firstnum
